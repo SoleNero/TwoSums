cb5ce81d4998ad2e118eaad0f30a7c9b15ffbdf0
Justyna Foster <justynawfoster@gmail.com>
2017-01-10 09:31:55 -0700

    change names

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# TwoSums
+# plusOneSum
+Warmup - Write a function that takes an array of integers and returns the sum of the integers after adding 1 to each. Write tests instead of console.logs.
+
+plusOneSum([1, 2, 3, 4]); // 14
--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Warmup - Write a function that takes an array of integers and returns the sum of the integers after adding 1 to each. Write tests instead of console.logs.
-//
-// plusOneSum([1, 2, 3, 4]); // 14
+
 'use strict';
 
 
